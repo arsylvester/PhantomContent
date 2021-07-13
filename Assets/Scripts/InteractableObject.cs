4375fcfca434ddc9ea0700ad40acc5a731e006a6
Noah Sims <noahsims24@gmail.com>
2021-07-13 01:01:20 -0500

    Began working on Inventory System

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
+    [Header ("Type")]
     public bool isNPC;
     public bool isPickUp;
+
+    [Header ("Status")]
+    public bool isAvailable = true;
+
+    private void Start()
+    {
+        isAvailable = true;
+    }
+
+    public void pickUpItem()
+    {
+        Debug.Log("Destroying item");
+        isAvailable = false;
+        this.gameObject.SetActive(false);
+    }
 }
