8f51c6388a152186d28ef8c6e0615a0b8f2a16a8
Andrew Sylvester <andrewrsylvester@gmail.com>
2021-07-20 20:32:29 -0500

    Escort mission fixes

--- Assets/Scripts/QuestMaster.cs
+++ Assets/Scripts/QuestMaster.cs
@@ -297,6 +297,9 @@ public class QuestMaster : MonoBehaviour
         storage.SetValue("$isDelivering", package);
         if(deliveryQuestStep == QuestStep.Completed)
             storage.SetValue("$delivery_complete", package);
+        //Escort
+        if (escortQuestStep == QuestStep.Completed)
+            storage.SetValue("$trip_finished", true);
     }
 
     public void FailDeliveryQuest()
